38fda5dd7380fb24e17969de94cf72545fb5e1c1
amina-a12 <aminatganiyu12@gmail.com>
2023-04-24 13:47:27 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printft("Size of a char: %d byte(s)", sizeof(char)));
+	printft("Size of an int: %d byte(s)", sizeof(int)    ));
+	 printft("Size of a long int: %d byte(s)", sizeof(long int)    ));
+	  printft("Size of a long long int: %d byte(s)", sizeof(long long int)    ));
+	   printft("Size of a float: %d byte(s)", sizeof(float)    ));
+    return (0);
+}
